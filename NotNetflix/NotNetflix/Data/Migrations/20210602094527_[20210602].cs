044a41005407ed01d2ea01c4d4f744b339ab366d
José Santos <aluno21892@ipt.pt>
2021-06-07 14:32:48 +0100

    Introdução de dados e ficheiros já é válida :)

---
+++ NotNetflix/NotNetflix/Data/Migrations/20210602094527_[20210602].cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NotNetflix.Data.Migrations
+{
+    public partial class _20210602 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
